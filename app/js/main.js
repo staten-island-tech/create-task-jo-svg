cad3c47d9685e2575dfb62d8c551dea30b7833ec
jo-svg <Josephinel43@nycstudents.net>
2025-01-02 23:27:16 -0500

    asdwaf

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,11 @@ import {cards} from './cards';
 
 function callFunctions(){
     document.querySelector(".playerCards").innerHTML = '';
+    addPlayerCards(playerCards);
+    console.log(playerCards);
+    addDealerCards(dealer);
+    console.log(dealer);
+    update();
     
 }
 function getRandomNumber(min, max) {
@@ -16,24 +21,56 @@ const dealer = [];
 function createCard(containerClass, num){
     const container = document.querySelector(`.${containerClass}`);
     container.insertAdjacentHTML("beforeend",`
-        <div class="card bg-slate-200 w-48 h-64 justify-center text-center mx-3">
+        <div class="card bg-slate-200 w-48 h-64 justify-center text-center">
           <h1 class="text-8xl">${num}</h1>
         </div>
         `);
 }
+function addCard(personsCards){
+    let newCardnum = getRandomNumber(1,10);
+    let cardValue = newCardnum;
+    if (newCardnum >= 10) {
+        cardValue = 10;
+    }
+    createCard('playerCards', cardValue);
+    personsCards.push(cardValue);
+    update();
+}
 
-function addCard(){
+function addPlayerCards(personsCards){
     const hitBtn = document.querySelector(".HIT");
     hitBtn.addEventListener('click', ()=>{
         console.log("New Card Is Added");
-        let newCardnum = getRandomNumber(1,10);
-        if (newCardnum){
-            createCard('playerCards', newCardnum);
-        }
+        addCard(personsCards);
     })
 }
- 
-addCard();
+function addDealerCards(personsCards){
+    addCard(personsCards);
+    
+}
+
+function update(){
+    let playervalue = 0; 
+    let dealervalue = 0;
+    for (let i=0; i < playerCards.length; i++){
+        playervalue += playerCards[i];
+    }
+    for (let i=0; i < dealer.length; i++){
+        dealervalue += dealer[i];
+    }
+    console.log(playervalue);
+    if (playervalue > 21){
+        console.log("you lose!");
+        document.querySelector(".HIT").disabled = true;
+    }
+    
+    /* if (dealervalue > 21 || playervalue > dealervalue){
+        console.log("you win!");
+        document.querySelector(".HIT").disabled = true;
+    } */
+}
+callFunctions();
+
 //need a funciton to create the cards
 //need a fucntion for storage
 //need a function for all the cards 
